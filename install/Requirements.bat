0ff37e2206bb6e35e78e949cdaac85ee92fb4d8e
Jinglin Mu <32406182+watfros@users.noreply.github.com>
2026-03-06 11:54:34 +0800

    Create Requirements.bat

---
+++ install/Requirements.bat
@@ -0,0 +1,8 @@
+python -m pip install numpy==1.22.4   
+python -m pip install PyQt5==5.15.4   
+python -m pip install matplotlib==3.5.2 
+python -m pip install traits==6.3.2  
+python -m pip install VTK==9.0.1 
+python -m pip install mayavi==4.7.4 
+
+pause
